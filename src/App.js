4401d90b0bde513e09e6f87e771ea93215eede3e
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-06-03 12:31:14 +0930

    work on the menu section of nav bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import BannerImage from "./images/Banner-Image.png";
 import AirbnbLogo from "./images/Airbnb_Logo.svg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import user from "./images/user.png";
 
 function App() {
   return (
@@ -19,18 +21,33 @@ function App() {
               </span>
             </div>
             <div className='navbar section-padding'>
-                  <img className='logo' src={AirbnbLogo} alt="airbnb log and icon"></img>
-                  <div className='search-bar'>
-                    <div className='search-buttons'>
-                      <div className='anywhere'>Anywhere</div>
-                      <div className='any-week'>Any week</div>
-                      <div className='add-guests'>Add Guests</div>
-                      <div className='button'>
-                        <FontAwesomeIcon className='icon' icon={faMagnifyingGlass} />
-                      </div>
+                <img className='logo' width="102" height="32" src={AirbnbLogo} alt="airbnb log and icon"></img>
+                <div className='search-bar'>
+                  <div className='search-buttons'>
+                    <div className='anywhere'>Anywhere</div>
+                    <div className='any-week'>Any week</div>
+                    <div className='add-guests'>Add Guests</div>
+                    <div className='button'>
+                      <FontAwesomeIcon className='search-icon' icon={faMagnifyingGlass} />
                     </div>
                   </div>
-                  <div>menu</div>
+                </div>
+                <div className='menu-bar'>
+                  <div className='airbnb-your-home'>
+                    <p className='text'>Airbnb your home</p>
+                    <div className='world-icon'>
+                      <img width="16" height="16" src="https://img.icons8.com/ios-glyphs/30/000000/globe-earth--v1.png" alt="line icon of earth globe"></img>
+                    </div>
+                  </div>
+                  <div className='menu'>
+                    <div>
+                      <FontAwesomeIcon className='menu-icon' icon={faBars} />
+                    </div>
+                    <div>
+                      <img className='user-icon' width="30" height="30" src={user} alt="user profile icon"></img>
+                    </div>
+                  </div>
+                </div>
             </div>
             <div className='category-bar'><div className='section-padding'>category scroll</div></div>
         </header>
